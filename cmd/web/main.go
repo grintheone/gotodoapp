12525a1e80bb5b2268eb41ad5a60d8a6c2dba2ea
grintheone <grin3run@gmail.com>
2024-03-10 12:12:39 +0300

    Initial setup, basic route

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,23 @@
+package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	host string = "localhost"
+	port string = ":3000"
+)
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", ping)
+	fmt.Printf("Server started on port %s\n", port)
+	log.Fatal(http.ListenAndServe(host+port, mux))
+}
